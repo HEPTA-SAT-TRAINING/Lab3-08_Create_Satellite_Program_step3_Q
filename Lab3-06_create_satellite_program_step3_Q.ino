4aa792d8e37067f218df0bb058d00a414da98f97
masa-night <csms21050@g.nihon-u.ac.jp>
2026-05-26 19:32:43 +0900

    Initial commit

---
+++ Lab3-06_create_satellite_program_step3_Q.ino
@@ -0,0 +1,70 @@
+#include "src/HeptaSat.h"
+
+HeptaCdh cdh;
+HeptaEps eps;
+
+const float temperature = 25.0; // Temperature in degrees Celsius
+
+bool is_cmd_received(void);
+char get_cmd(void);
+
+void setup() {
+  cdh.begin();
+  eps.init();
+  cdh.wait_for_serial();
+  cdh.wait_for_sd();
+}
+
+void loop() {
+  cdh.println("------------------------------");
+  cdh.printf("Satellite Time: %.2f seconds\n", millis() / 1000.0); // Print time in seconds
+  cdh.printf("Battery Voltage: %.2f V\n", eps.get_battery_voltage());
+  cdh.printf("Temperature: %.2f °C\n", temperature);
+  cdh.println("------------------------------");
+
+  if (is_cmd_received()) {
+    char cmd = get_cmd();
+
+    if (cmd != '\0') {
+      cdh.printf("Received command: %c\n", cmd);
+
+      // Process the command and respond accordingly
+      switch (cmd) {
+        case 'a': {
+          for(uint8_t i = 0; i < 10; i++) {
+            cdh.println("Hello Hepta sat lite");
+            delay(1000);
+          }
+          break;
+        }
+
+        case 'b': {
+          // Write some code to measure the battery voltage and save it to a file on the SD card
+          // You can refer to Lab3-04_save_battery_voltage_to_microsd
+
+        }
+
+        default:
+          cdh.println("Unknown command");
+          break;
+      }
+    }
+  }
+  delay(1000);
+}
+
+bool is_cmd_received(void) {
+  // Check if there is any data available in the serial buffer
+  return Serial.available() > 0;
+}
+
+char get_cmd(void) {
+  // Read one command character and ignore line endings from Serial Monitor.
+  while (is_cmd_received()) {
+    char received_char = Serial.read();
+    if (received_char != '\r' && received_char != '\n') {
+      return received_char;
+    }
+  }
+  return '\0'; // Return null character if no data is available
+}
